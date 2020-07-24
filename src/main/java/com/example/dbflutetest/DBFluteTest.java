df99fc515cd202569b9393226f9abf812a5a418a
KOMORI Yusuke(y-komori@altemis) <komo@littleforest.jp>
2020-07-24 12:33:22 +0900

    Fix test code

--- src/main/java/com/example/dbflutetest/DBFluteTest.java
+++ src/main/java/com/example/dbflutetest/DBFluteTest.java
@@ -2,8 +2,7 @@ package com.example.dbflutetest;
 
 import com.example.dbflutetest.dbflute.exbhv.MemberBhv;
 import com.example.dbflutetest.dbflute.exentity.Member;
-import org.dbflute.cbean.result.ListResultBean;
-import org.dbflute.optional.OptionalEntity;
+import org.dbflute.cbean.result.PagingResultBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -18,12 +17,47 @@ public class DBFluteTest {
     public static void main(String[] args) {
 		ConfigurableApplicationContext ctx = SpringApplication.run(DBFluteTest.class, args);
 		DBFluteTest app = ctx.getBean(DBFluteTest.class);
-		app.run();
+		app.testPage();
+		app.testPageByOffset();
 	}
 
-	public void run(){
-		System.out.println("OK");
-		ListResultBean<Member> members = memberBhv.selectList(cb -> cb.paging(10, 2));
-		members.forEach(System.out::println);
+	public void testPage(){
+		System.out.println("testPage =============================================================================");
+		int pageSize = 33;
+		int pageNum = 1;
+		do {
+		} while (showPage(pageSize, pageNum++));
+		System.out.println();
+	}
+
+	private boolean showPage(final int pageSize, final int pageNum){
+		PagingResultBean<Member> page = memberBhv.selectPage(cb -> cb.paging(pageSize, pageNum));
+		System.out.printf("PageSize: %3d, PageNum: %3d, AllPageCount: %3d, AllRecordCount: %4d, FetchRecords: %3d\n",
+				pageSize, pageNum, page.getAllPageCount(), page.getAllRecordCount(), page.size());
+//		page.forEach(System.out::println);
+		return page.existsNextPage();
+	}
+
+	public void testPageByOffset(){
+		System.out.println("testPageByOffset =====================================================================");
+		int pageSize = 30;
+		int offset = 0;
+		PagingResultBean<Member> page = null;
+		do {
+			page = showPageByOffset(offset, pageSize);
+			offset += page.size();
+		} while (page.existsNextPage());
+		System.out.println();
+	}
+
+	private PagingResultBean<Member> showPageByOffset(final int offset, final int pageSize){
+		PagingResultBean<Member> page = memberBhv.selectPage(cb -> {
+			cb.xfetchScope(offset, pageSize);
+			cb.disablePagingCountLater();
+		});
+		System.out.printf("PageSize: %3d, Offset: %3d, AllPageCount: %3d, AllRecordCount: %4d, FetchRecords: %3d\n",
+				pageSize, offset, page.getAllPageCount(), page.getAllRecordCount(), page.size());
+//		page.forEach(System.out::println);
+		return page;
 	}
 }
